63501a0d8ca01251cb5ab7cb626fec5088e745ab
ToritsejuEyekosi <tjeyeks@gmail.com>
2023-02-19 20:03:08 -0800

    5am

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+int main(void)
+{
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
+Return(0);
+}
