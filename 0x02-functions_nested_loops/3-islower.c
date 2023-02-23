aeffe38fdd1e9041863eaa182ef664085446e7ac
ToritsejuEyekosi <tjeyeks@gmail.com>
2023-02-22 16:14:06 -0800

    moving on

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+int islower(int c)
+{
+if (c >= 97 && c <= 122)
+{return (1);}
+return (0);
+}
