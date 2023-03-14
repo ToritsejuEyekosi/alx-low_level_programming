b7b378338ff67ac465601ada9bdf7ca431593a70
ToritsejuEyekosi <tjeyeks@gmail.com>
2023-03-13 21:33:22 -0700

    everything at once

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,12 +1,14 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * File: holberton.h
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0A-malloc_free directory.
  */
-
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
